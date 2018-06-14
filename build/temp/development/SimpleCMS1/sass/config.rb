ab94b9f3afbdf7a32de7d9536d12dda1a4635084
Administrator <Administrator@XHJ>
2018-06-14 11:12:22 +0800

    添加项目文件。

---
+++ build/temp/development/SimpleCMS1/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\Administrator\source\repos\SimpleCMS-1/build/.sass-cache'
---
+++ build/temp/development/SimpleCMS1/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Administrator\source\repos\SimpleCMS-1\app.js
